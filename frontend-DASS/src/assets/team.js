30d5336370a9b0e899b9c66d2e3e9a6937678ac9
Devagya-Singh <devagyarupsingh.com>
2025-04-09 13:38:41 +0530

    Initial commit

---
+++ frontend-DASS/src/assets/team.js
@@ -0,0 +1,42 @@
+import adrija from './profile_adrija.jpg'
+import ankita from './profile_ankita.jpg'
+import debleena from './profile_deebleena.jpg'
+import devagya from './profile_devagya.jpg'
+import sampurna from './profile_sampurna.jpg'
+import swaralipi from './profile_swaralipi.jpg'
+
+const teamMembers = [
+    {
+      name: "Devagya Singh",
+      role: "Project Lead, Frontend Engineer",
+      image: devagya // Ensure this path is correct
+    },
+    {
+      name: "Swarlipi Samanta",
+      role: "Frontend Engineer, Integration Tester",
+      image: swaralipi
+    },
+    {
+      name: "Sampurna Sen",
+      role: "Technical Writer",
+      image: sampurna
+    },
+    {
+      name: "Debleena Biswas",
+      role: "UI/UX Designer",
+      image: debleena
+    },
+    {
+      name: "Adrija Das",
+      role: "QA & Testing",
+      image: adrija
+    },
+    {
+        name: "Ankita Mohanty",
+        role: "QA & Testing",
+        image: ankita
+      }
+  ];
+  
+  export default teamMembers;
+  
